a11d30e8b19e1c2260ff64c91e174038ab215d61
lance13c <pianoman13c@gmail.com>
2016-11-14 23:16:53 -0500

    init project add SearchBar SearchBar onInputChange bind this SearchBar onInputChange updates state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,3 +1,38 @@
 /**
  * Created by Lance on 11/14/2016.
  */
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = { term: ''};
+		
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+	
+	/**
+	 *
+	 * @param event
+	 */
+	onInputChange(event) {
+		console.log(event.target.value);
+		this.setState({ term: event.target.value});
+	}
+	
+	render() {
+			return (
+				<form className="input-group">
+					<input
+						placeholder="Get forecasts for your favorite cities"
+						className="form-control"
+						value={this.state.term}
+						onChange={this.onInputChange}/>
+					<span className="input-group-btn">
+						<button type="submit" className="btn btn-secondary">Submit</button>
+					</span>
+				</form>
+			);
+	}
+}
